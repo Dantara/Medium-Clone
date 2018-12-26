1be98c7eecf284141c268b5d3236a858811a7bfa
Dantara <supercreed2@gmail.com>
2018-12-26 19:18:50 +0300

    Tests refactoring

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,16 +1,13 @@
 require 'rails_helper'
 
-RSpec.configure do |config|
-  config.include Devise::Test::ControllerHelpers, type: :controller
-end
-
 RSpec.describe HomeController, type: :controller do
 
   describe "GET #index" do
    it "redirects when user not specified" do
      get :index
-     expect(response).to have_http_status(:redirect)
+     expect(response).to redirect_to("/users/sign_in")
    end
+
   end
 
 end
